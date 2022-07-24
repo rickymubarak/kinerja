447939e165f3fc8dccf469790e205a85ca7a6058
rickymubarak <rickymubarak.rm@gmail.com>
2022-07-24 15:29:12 +0700

    menambah navbar

--- home.php
+++ home.php
@@ -16,6 +16,14 @@
     <title>Pelaporan Kinerja</title>
 </head>
 <body>
+
+    <?php
+        include "navbar.php"
+    ?>
+
+    <!-- akhir halaman navbar -->
+
+    <!-- halaman konten -->
     <div class="fContainer">
         <div class="wrapper">
             <h1>InI HOME</h1>
@@ -24,6 +32,8 @@
             </div>
         </div>
     </div>
+
+    <!-- akhir halaman konten -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
---
+++ navbar.php
@@ -0,0 +1,31 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Style dari bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<!-- Css Custome -->
+    <link rel="stylesheet" href="plugin/style.css">
+
+    <title>Pelaporan Kinerja</title>
+</head>
+
+<body>
+	<!-- navbar -->
+    <section>
+        <div class="navbar">
+            <ul>
+                <li>Dashboard</li>
+                <li>Profile</li>
+                <li>Laporan Kinerja</li>
+                <li>Rekapan Produktivitas</li>
+            </ul>
+        </div>    
+    </section>
+</body>
