e48b7a682f04d3440ef1569e36a148cb68b96d8a
rickymubarak <rickymubarak.rm@gmail.com>
2022-08-12 04:43:05 +0700

    menambah authentikasi login

--- resources/views/rencana/index.blade.php
+++ resources/views/rencana/index.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Kinerja</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
 </head>
 
+@extends('layouts.app')
+@section('content')
+    
     <div class="card p-5">
         <div>
             <a href="/rencana/create" class="btn btn-primary col-sm-2">Tambah Rencana</a>
@@ -88,6 +88,7 @@
         </div>
     </div>
 
+@endsection
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -14,30 +14,34 @@ use App\Http\Controllers\RencanaController;
 |
 */
 
-Route::any('/', function() {
-    return view('index', [
-        'style' => "style.css"
-    ]);
+
+Route::middleware(['auth'])->group(function(){
+    Route::any('/', function() {
+        return view('home');
+    });
+    Route::get('/rencana',[RencanaController::class,'index']);
+    Route::any('/rencana/create', [RencanaController::class,'create']);
+    Route::any('/rencana/store', [RencanaController::class,'store']);
+
+    // route mengarahkan edit
+    Route::get('/rencana/edit/{id}', [RencanaController::class,'edit']);
+
+    // route aksi edit
+    Route::put('/rencana/{id}', [RencanaController::class,'update']);
+
+    // route delete
+    Route::any('/rencana/hapus/{id}',[RencanaController::class,'hapus']);
+
+    // route menampilkan softdelete
+    Route::get('/rencana/recycle',[RencanaController::class,'recycle']);
+
+    // route menjalankan aksi restore
+    Route::any('/rencana/restore/{id}',[RencanaController::class,'restore']);
+
+    // route validasi panggil
+    Route::get('rencana/validasi/{id}',[RencanaController::class,'validasi']);
 });
 
-Route::any('/rencana', [RencanaController::class,'index']);
-Route::any('/rencana/create', [RencanaController::class,'create']);
-Route::any('/rencana/store', [RencanaController::class,'store']);
+Auth::routes();
 
-// route mengarahkan edit
-Route::get('/rencana/edit/{id}', [RencanaController::class,'edit']);
-
-// route aksi edit
-Route::put('/rencana/{id}', [RencanaController::class,'update']);
-
-// route delete
-Route::any('/rencana/hapus/{id}',[RencanaController::class,'hapus']);
-
-// route menampilkan softdelete
-Route::get('/rencana/recycle',[RencanaController::class,'recycle']);
-
-// route menjalankan aksi restore
-Route::any('/rencana/restore/{id}',[RencanaController::class,'restore']);
-
-// route validasi panggil
-Route::get('rencana/validasi/{id}',[RencanaController::class,'validasi']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
