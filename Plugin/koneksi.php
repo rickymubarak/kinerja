a7bbd9dcdab5d170babdbaaa18e572d45c3ae7ae
rickymubarak <rickymubarak.rm@gmail.com>
2022-09-04 00:08:22 +0700

    fitur reset via mailtrap.io

--- Plugin/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// koneksi database
-	$server = "localhost";
-	$user	= "root";
-	$pass	= "";
-	$database	= "kinerja";
-	$koneksi	= mysqli_connect($server, $user, $pass, $database) or die(mysqli_error($koneksi));
-?>
